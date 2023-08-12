fbfbb37246be9f96207ebc885d7fdd30eb23e674
cpufilecode <zf0908@naver.com>
2023-08-12 14:54:34 +0900

    upload

---
+++ src/main/kotlin/02/XorSwapTest.kt
@@ -0,0 +1,13 @@
+package `02`
+
+fun main() {
+    var number1 = 12
+    var number2 = 25
+
+    number1 = number1 xor number2
+    number2 = number1 xor number2
+    number1 = number1 xor number2
+
+    println("number1 = " + number1)
+    println("number2 = " + number2)
+}
