14303b38b0933da022313ba6ded3d538e7b65f92
cpufilecode <zf0908@naver.com>
2023-08-15 23:05:28 +0900

    upload

---
+++ src/main/kotlin/02/ValVar.kt
@@ -0,0 +1,13 @@
+package `02`
+
+fun main()
+{
+    val number = 10
+    var language = "Korean"
+    val secondNumber: Int = 20
+    language = "English"
+
+    println("number: $number")
+    println("language: $language")
+    println("secondNumber: $secondNumber")
+}
