bb0ec25b2f17d20ecae395771abb6931fa6f44f0
cpufilecode <zf0908@naver.com>
2023-08-16 08:39:36 +0900

    upload

--- src/main/kotlin/03/DefaultParameter.kt
+++ src/main/kotlin/03/DefaultParameter.kt
@@ -12,6 +12,7 @@ fun main() {
 
 fun add(name: String, email: String = "default") {
     val output = "${name}님의 이메일은 ${email}입니다."
+    println(output)
 }
 
 fun defaultArgs(x: Int = 100, y: Int = 200) {
