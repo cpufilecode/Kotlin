f421da53907a58b4fc8baeaa84f644a746d45daf
cpufilecode <zf0908@naver.com>
2023-08-16 08:37:08 +0900

    upload

--- src/main/kotlin/03/DefaultParameter.kt
+++ src/main/kotlin/03/DefaultParameter.kt
@@ -3,12 +3,17 @@ package `03`
 fun main() {
     val name = "홍길동"
     val email = "hong@example.kr"
+
+    add(name)
+    add(name, email)
+    defaultArgs()
+    defaultArgs(200)
 }
 
 fun add(name: String, email: String = "default") {
 
 }
 
-fun defaultArgss(x: Int = 100, y: Int = 200) {
-
+fun defaultArgs(x: Int = 100, y: Int = 200) {
+    println(x+y)
 }
