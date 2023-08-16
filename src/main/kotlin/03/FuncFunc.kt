cf8336e2f03f6db2907167b439b448a9c9658dcc
cpufilecode <zf0908@naver.com>
2023-08-16 12:53:02 +0900

    upload

--- src/main/kotlin/03/FuncFunc.kt
+++ src/main/kotlin/03/FuncFunc.kt
@@ -1,2 +1,11 @@
 package `03`
 
+fun main() {
+    println("funcFunc: ${funcFunc()}")
+}
+
+fun sum(a: Int, b: Int) = a + b
+
+fun funcFunc(): Int {
+    return sum(2,2)
+}
--- src/main/kotlin/03/sumFunc.kt
+++ src/main/kotlin/03/sumFunc.kt
@@ -6,6 +6,7 @@ fun sum(a: Int, b: Int): Int{
     return sum
  */
 
+/*
 fun main() {
     val result1 = sum(3,2)
     val result2 = sum(6,7)
@@ -13,3 +14,4 @@ fun main() {
     println(result1)
     println(result2)
 }
+*/
