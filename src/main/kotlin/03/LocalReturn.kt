dd9efa8178115bcd9320c850d381537b53dfbc78
cpufilecode <zf0908@naver.com>
2023-08-16 15:08:12 +0900

    upload

--- src/main/kotlin/03/LocalReturn.kt
+++ src/main/kotlin/03/LocalReturn.kt
@@ -1,2 +1,14 @@
 package `03`
 
+fun main() {
+    shortFunc(3) {
+        println("First call: $it")
+        return // ① 의도하지 않은 반환
+    }
+}
+
+inline fun shortFunc(a: Int, out: (Int) -> Unit) {
+    println("Before calling out()")
+    out(a)
+    println("After calling out()") // ② 이 문장은 실행되지 않음
+}
